2bdb5c9b703455f486b1c4d18fe2a8895ecbd580
Luciano Baraglia <luciano.baraglia@gmail.com>
2013-08-09 17:50:41 -0300

    Avances?

--- huayra_simulaciones_interactivas/escenas/EscenaCategorias.py
+++ huayra_simulaciones_interactivas/escenas/EscenaCategorias.py
@@ -9,7 +9,8 @@ from ..NavegacionCategorias import NavegacionCategorias
 class EscenaCategorias(pilas.escena.Base):
 	
 	nav = None  # Navegación de categorías
-	new_x, end_x = 0, 0
+	v = 0.15  # Escala de velocidad
+	vx = 0
 	
 	def __init__(self):
 		Base.__init__(self)
@@ -47,10 +48,14 @@ class EscenaCategorias(pilas.escena.Base):
 		pilas.escena_actual().mueve_mouse.conectar(self.mover)
 	
 	
-	def mover(self, evento):		
-		self.end_x = evento.x
-		self.new_x = self.new_x + (self.end_x - self.new_x) * 0.09
-		self.nav.x = -(self.new_x)
-		print "Nav X:", self.nav.x
-		print "mouse X:", evento.x
+	def mover(self, evento):
+		self.vx =  -(self.v * evento.x)
+		if self.vx > 5: self.vx = 5
+		if self.vx < -5: self.vx = -5
+		#self.nav.x = pilas.interpolar(-(evento.x*2), 1)
+		self.nav.x += self.vx
+		#print "Dist x:", dx
+		#print "V:", vx
+		#print "Nav X:", self.nav.x
+		#print "mouse X:", evento.x
 		
--- huayra_simulaciones_interactivas/simulaciones.py
+++ huayra_simulaciones_interactivas/simulaciones.py
@@ -126,5 +126,129 @@ simulaciones = {
 				},
 			],
 		}, # fin categoría Biología
+		{
+			'id': 'fisica', 
+			'nombre': u'Física',
+			'screenshot': 'imagenes/screenshots/fisica.png',
+			'descripcion': 'Lorem ipsum dolor sit amet, consetetur',
+			'actor': None,
+			'subcategorias': [
+				{
+					'id': 'movimiento',
+					'nombre': 'Movimiento',
+					'screenshot': 'imagenes/screenshots/movimiento.png',
+					'actor': None,
+					'simulaciones': [
+						{
+							'id': 'fuerzas-movimiento-fundamentos',
+							'nombre': 'Fuerzas y Movimiento: Fundamentos',
+							'screenshot': 'imagenes/screenshots/forces-and-motion-basics-screenshot.png',
+							'descripcion': 'Explora las fuerzas que actúan en un tira y afloja o empuje un refrigerador, caja, o una persona. Crea una fuerza aplicada y ver cómo se hace mover objetos. Cambia la fricción y observa cómo afecta el movimiento de los objetos.',
+							'file': '---',
+							'actor': None
+						},
+					]
+				},
+				{
+					'id': 'sonido_ondas',
+					'nombre': 'Sonido &amp; ondas',
+					'screenshot': 'imagenes/screenshots/sonido_ondas.png',
+					'actor': None,
+					'simulaciones': [
+						{
+							'id': 'frecuencias-resonantes',
+							'nombre': 'Frecuencias resonantes',
+							'screenshot': 'imagenes/screenshots/normal-modes-screenshot.png',
+							'descripcion': 'Juega con un sistema de 1D o 2D de resortes osciladores acoplados. Varía el número de masas, ajusta las condiciones iniciales, y observa que el sistema evolucione. Ve todo el espectro de frecuencias resonantes del movimiento arbitrario. Véase las frecuencias longitudinales y transversales en el sistema 1D.',
+							'file': '---',
+							'actor': None
+						},
+						{
+							'id': 'interferencia-onda',
+							'nombre': 'Interferencia De la Onda',
+							'screenshot': 'imagenes/screenshots/wave-interference-screenshot.png',
+							'descripcion': 'Haz ondas con un grifo que gotea, altavoces de audio, o láser! Añade una segunda fuente o un par de rendijas para crear un patrón de interferencia.',
+							'file': '---',
+							'actor': None
+						},
+					]
+				},
+			],
+		}, # fin categoría Física
+		{
+			'id': 'biologia', 
+			'nombre': u'Biología',
+			'screenshot': 'imagenes/screenshots/forces-and-motion-basics-screenshot.png',
+			'descripcion': 'sed diam nonumyeirmod tempor invidunt',
+			'actor': None,
+			'subcategorias': [
+				{
+					'id': None,
+					'nombre': None,
+					'screenshot': 'imagenes/screenshots/movimiento.png',
+					'actor': None,
+					'simulaciones': [
+						{
+							'id': 'fuerzas-movimiento-fundamentos',
+							'nombre': 'Fuerzas y Movimiento: Fundamentos',
+							'screenshot': 'imagenes/screenshots/forces-and-motion-basics-screenshot.png',
+							'descripcion': 'Explora las fuerzas que actúan en un tira y afloja o empuje un refrigerador, caja, o una persona. Crea una fuerza aplicada y ver cómo se hace mover objetos. Cambia la fricción y observa cómo afecta el movimiento de los objetos.',
+							'file': '---',
+							'actor': None
+						},
+					]
+				},
+			],
+		}, # fin categoría Biología
+		{
+			'id': 'biologia', 
+			'nombre': u'Biología',
+			'screenshot': 'imagenes/screenshots/forces-and-motion-basics-screenshot.png',
+			'descripcion': 'sed diam nonumyeirmod tempor invidunt',
+			'actor': None,
+			'subcategorias': [
+				{
+					'id': None,
+					'nombre': None,
+					'screenshot': 'imagenes/screenshots/movimiento.png',
+					'actor': None,
+					'simulaciones': [
+						{
+							'id': 'fuerzas-movimiento-fundamentos',
+							'nombre': 'Fuerzas y Movimiento: Fundamentos',
+							'screenshot': 'imagenes/screenshots/forces-and-motion-basics-screenshot.png',
+							'descripcion': 'Explora las fuerzas que actúan en un tira y afloja o empuje un refrigerador, caja, o una persona. Crea una fuerza aplicada y ver cómo se hace mover objetos. Cambia la fricción y observa cómo afecta el movimiento de los objetos.',
+							'file': '---',
+							'actor': None
+						},
+					]
+				},
+			],
+		}, # fin categoría Biología
+		{
+			'id': 'biologia', 
+			'nombre': u'Biología',
+			'screenshot': 'imagenes/screenshots/forces-and-motion-basics-screenshot.png',
+			'descripcion': 'sed diam nonumyeirmod tempor invidunt',
+			'actor': None,
+			'subcategorias': [
+				{
+					'id': None,
+					'nombre': None,
+					'screenshot': 'imagenes/screenshots/movimiento.png',
+					'actor': None,
+					'simulaciones': [
+						{
+							'id': 'fuerzas-movimiento-fundamentos',
+							'nombre': 'Fuerzas y Movimiento: Fundamentos',
+							'screenshot': 'imagenes/screenshots/forces-and-motion-basics-screenshot.png',
+							'descripcion': 'Explora las fuerzas que actúan en un tira y afloja o empuje un refrigerador, caja, o una persona. Crea una fuerza aplicada y ver cómo se hace mover objetos. Cambia la fricción y observa cómo afecta el movimiento de los objetos.',
+							'file': '---',
+							'actor': None
+						},
+					]
+				},
+			],
+		}, # fin categoría Biología
 	], # fin categorías
 }
