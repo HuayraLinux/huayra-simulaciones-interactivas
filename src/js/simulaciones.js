cc838489349c689c56c6a52224878ea53ee4fbc5
Mauro <lavaramano@gmail.com>
2015-05-07 17:24:08 -0300

    filtra simulaciones, falta estetica

--- src/js/simulaciones.js
+++ src/js/simulaciones.js
@@ -1,5 +1,6 @@
 var exec = require('child_process').exec;
 var sim_path = "/usr/share/huayra-simulaciones-interactivas/data/simulaciones/";
+var scr_path = "file:///usr/share/huayra-simulaciones-interactivas/data/screenshots"
 var java_jar = "java -jar ";
 
 function load_simus(content){
@@ -7,6 +8,7 @@ function load_simus(content){
 
     for(obj in categories){
         categories[obj]['simus'] = categories[obj]['simus'].map(function(k){ return simulations[k]; });
+        categories[obj]['scr_path'] = scr_path;
         categories[obj]['cant'] = categories[obj]['simus'].length;
         content.append(
             Mustache.render($('#tmpl-category').html(), categories[obj])
@@ -33,11 +35,32 @@ function open_simulation(sim_slide){
 }
 
 
+function filter_sim(input){
+    Reveal.setState(7);
+    console.log( input.val() );
+
+    console.log("//*//*[contains(title, '_STR_')]".replace('_STR_',
+                                                           input.val()));
+
+    var s_res = JSON.search(simulations,
+                            "//*//*[contains(title, '_STR_')]//file".replace('_STR_',
+                                                                             input.val()));
+    categories["filtrar"]['simus'] = s_res.map(function(s){ return s.replace('_es.jar', ''); });
+    load_simus($('#content'));
+}
+
 $(document).ready(function(){
     load_simus($('#content'));
 
     $('.simudescription').on('click', function(){
       open_simulation($(this));
+    });
+
+    $('#btn-filter-sim').on('click', function(){
+      filter_sim($('#filter-sim'));
+    });
+    $('#filter-sim').on('keyup', function(){
+      filter_sim($(this));
     });
 
     Reveal.initialize({
