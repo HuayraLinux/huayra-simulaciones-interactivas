bd9d5297fdc2813c3a6074072b5126b022f6deed
Mauro <lavaramano@gmail.com>
2015-05-08 17:35:49 -0300

    mini cambios

--- src/js/simulaciones.js
+++ src/js/simulaciones.js
@@ -4,12 +4,15 @@ var scr_path = "file:///usr/share/huayra-simulaciones-interactivas/data/screensh
 var java_jar = "java -jar ";
 
 function load_simus(content){
-    content.html('');
-
     for(obj in categories){
-        categories[obj]['simus'] = categories[obj]['simus'].map(function(k){ if( typeof(k) == "string" ) {s = simulations[k]; s.name = k; return s;} });
+        console.log(obj);
+
+        categories[obj]['simus'] = categories[obj]['simus'].map(function(k){ s = simulations[k]; s.name = k; return s; });
         categories[obj]['scr_path'] = scr_path;
         categories[obj]['cant'] = categories[obj]['simus'].length;
+        categories[obj]['key'] = obj;
+
+
         content.append(
             Mustache.render($('#tmpl-category').html(), categories[obj])
         );
@@ -36,33 +39,29 @@ function open_simulation(sim_slide){
 
 }
 
+function fn_filter_sim(){
+    filter_sim($('#filter-sim'));
+}
 
 function filter_sim(input){
-    Reveal.setState(7);
-    console.log( input.val() );
-
     console.log("//*//*[contains(title, '_STR_')]".replace('_STR_',
                                                            input.val()));
 
     var s_res = JSON.search(simulations,
                             "//*//*[contains(title, '_STR_')]//file".replace('_STR_',
                                                                              input.val()));
-    //categories["filtrar"]['simus'] = s_res.map(function(s){ return s.replace('_es.jar', ''); });
     categories.filtrar.simus = s_res.map(function(s){ return s.replace('_es.jar', ''); });
     load_simus($('#content'));
 }
+
 
 $(document).ready(function(){
     load_simus($('#content'));
 
     $('.simudescription').on('click', fn_open_simulation);
 
-    $('#btn-filter-sim').on('click', function(){
-      filter_sim($('#filter-sim'));
-    });
-    $('#filter-sim').on('keyup', function(){
-      filter_sim($(this));
-    });
+    $('#btn-filter-sim').on('click', fn_filter_sim);
+    //$('#filter-sim').on('keyup', fn_filter_sim);
 
     Reveal.initialize({
         controls: true,
