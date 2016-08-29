a400825a2dd535eb361d25a2a1672d8de82ee076
Ignacio Losiggio <iglosiggio@gmail.com>
2016-08-29 10:05:16 -0300

    Agregado WM_CLASS

--- app/src/js/simulaciones.js
+++ app/src/js/simulaciones.js
@@ -2,6 +2,8 @@ var exec = require('child_process').exec;
 var sim_path = "/usr/share/huayra-simulaciones-interactivas/data/simulaciones/";
 var java_jar = "java -jar ";
 
+process.mainModule.exports.init(require('nwjs-hack').set_wmclass.bind(null, "huayra-simulaciones-interactivas", true));
+
 
 function load_simus(content){
     content.html('');
